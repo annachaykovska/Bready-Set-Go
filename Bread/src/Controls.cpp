107fffc938a3e9d7def7f4b88c558371940604c7
annachaykovska <anna.chayk@gmail.com>
2022-01-25 23:10:40 -0700

    smooth 2D movement

--- Bread/src/Controls.cpp
+++ Bread/src/Controls.cpp
@@ -11,34 +11,66 @@ using namespace std;
 *		- Track which players input is using the callback 
 */
 
+struct ButtonState {
+	bool forwardsHeld = false;
+	bool leftHeld = false;
+	bool backwardsHeld = false;
+	bool rightHeld = false;
+};
+	
+
 class MovementCallbacks : public CallbackInterface {
 
 public:
 	// Currently just taking a pointer to the main player transform????
-	MovementCallbacks(shared_ptr<Transform> transform) : transform(transform) {}
+	MovementCallbacks(shared_ptr<Transform> transform) : transform(transform) {
+		state = ButtonState();
+	}
 
 	virtual void keyCallback(int key, int scancode, int action, int mods) {
-		if (key == GLFW_KEY_W && action == GLFW_PRESS) {
+		// Pressing settings
+		if (state.forwardsHeld || (key == GLFW_KEY_W && action == GLFW_PRESS)) {
 			transform->position.x += 0.5;
+			state.forwardsHeld = true;
 			printf("W");
 		}
-		if (key == GLFW_KEY_A && action == GLFW_PRESS) {
+		if (state.leftHeld || (key == GLFW_KEY_A && action == GLFW_PRESS)) {
 			transform->position.z -= 0.5;
+			state.leftHeld = true;
 			printf("A");
 		}
-		if (key == GLFW_KEY_S && action == GLFW_PRESS) {
+		if (state.backwardsHeld || (key == GLFW_KEY_S && action == GLFW_PRESS)) {
 			transform->position.x -= 0.5;
+			state.backwardsHeld = true;
 			printf("S");
 		}
-		if (key == GLFW_KEY_D && action == GLFW_PRESS) {
+		if (state.rightHeld || (key == GLFW_KEY_D && action == GLFW_PRESS)) {
 			transform->position.z += 0.5;
+			state.rightHeld = true;
 			printf("D");
 		}
+
+		// Release settings
+		if (key == GLFW_KEY_W && action == GLFW_RELEASE) {
+			state.forwardsHeld = false;
+		}
+		if (key == GLFW_KEY_A && action == GLFW_RELEASE) {
+			state.leftHeld = false;
+		}
+		if (key == GLFW_KEY_S && action == GLFW_RELEASE) {
+			state.backwardsHeld = false;
+		}
+		if (key == GLFW_KEY_D && action == GLFW_RELEASE) {
+			state.rightHeld = false;
+		}
+
+		// Update position in transform
 		transform->position;
 	}
 
 	
 
 private:
+	ButtonState state;
 	shared_ptr<Transform> transform;
 };
