78edc674c1acaf4106a9103a308b07dd4f5a61e4
annachaykovska <anna.chayk@gmail.com>
2022-02-22 22:11:09 -0700

    cleanup

--- Bread/src/Physics/PhysicsEnums.h
+++ Bread/src/Physics/PhysicsEnums.h
@@ -18,25 +18,3 @@ enum DriveMode
 	eDRIVE_MODE_NONE
 };
 
-// Button State
-struct ButtonState {
-	// Single player controls (keyboard)
-	bool forwardsHeld = false;
-	bool leftHeld = false;
-	bool backwardsHeld = false;
-	bool rightHeld = false;
-	bool brakeHeld = false;
-
-	// Player 1 controller
-	float accelPlayer1 = 0.0;
-	float brakePlayer1 = 0.0;
-	float turnPlayer1 = 0.0;
-
-	// Camera
-	bool cameraForward = false;
-	bool cameraBackward = false;
-	bool cameraRight = false;
-	bool cameraLeft = false;
-	bool cameraUp = false;
-	bool cameraDown = false;
-};
