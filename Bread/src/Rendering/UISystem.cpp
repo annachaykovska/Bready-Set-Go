07cba9cf99afd1e6201046d986c357f560f32fed
derek-name <derek.braun.99@gmail.com>
2022-03-06 16:50:23 -0700

    Change sample text and re-add rendering

--- Bread/src/Rendering/UISystem.cpp
+++ Bread/src/Rendering/UISystem.cpp
@@ -89,8 +89,7 @@ UISystem::~UISystem(){
 }
 
 void UISystem::update() {
-    renderText(textShader, "This is sample text", 25.0f, 25.0f, 1.0f, glm::vec3(0.5, 0.8f, 0.2f));
-	//std::cout << "Update" << std::endl;
+    renderText(textShader, "Bready Set Go!!!", 25.0f, 25.0f, 1.0f, glm::vec3(0.5, 0.8f, 0.2f));
 }
 
 void UISystem::renderText(Shader& s, std::string text, float x, float y, float scale, glm::vec3 color) 
