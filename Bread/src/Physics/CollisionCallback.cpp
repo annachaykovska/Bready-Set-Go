de16d7caf31339e90b2ef51c1e773bd43bf1656e
annachaykovska <anna.chayk@gmail.com>
2022-03-11 19:47:25 -0700

    clean up collision callback

--- Bread/src/Physics/CollisionCallback.cpp
+++ Bread/src/Physics/CollisionCallback.cpp
@@ -32,95 +32,19 @@ void CollisionCallback::onContact(const physx::PxContactPairHeader& pairHeader,
     if (entity0->name == "player1" || entity0->name == "player2" || entity0->name == "player3" || entity0->name == "player4")
     {
         Inventory* playerInventory = (Inventory*)entity0->getComponent("inventory");
-        
-        if (entity1->name == "tomato" && playerInventory->tomato == 0)
-        {
-            playerInventory->tomato = 1;
-            entity0->getAudioSource()->play("pickup.wav");
-            //entity1->getModel()->visible = false;
-            entity1->removeFlag = true;
-        }
-        if (entity1->name == "cheese" && playerInventory->cheese == 0)
-        {
-            playerInventory->cheese = 1;
-            entity0->getAudioSource()->play("pickup.wav");
-            //entity1->getModel()->visible = false;
-            entity1->removeFlag = true;
-        }
-        if (entity1->name == "dough" && playerInventory->dough == 0)
-        {
-            playerInventory->dough = 1;
-            entity0->getAudioSource()->play("pickup.wav");
-            //entity1->getModel()->visible = false;
-            entity1->removeFlag = true;
-        }
-        if (entity1->name == "sausage" && playerInventory->sausage == 0) {
-            playerInventory->sausage = 1;
-            entity0->getAudioSource()->play("pickup.wav");
-            //entity1->getModel()->visible = false;
-            entity1->removeFlag = true;
-        }
-
-        if (playerInventory->checkPizza())
-            entity0->getAudioSource()->play("ding.wav");
-
+        // FOOD
+        entity0->checkIngredientCollision(entity1);
         // OTHER PLAYERS
-        vector<string> otherPlayers = entity0->getOtherPlayers();
-        if (entity1->name == otherPlayers[0] || entity1->name == otherPlayers[1] || entity1->name == otherPlayers[2])
-        {
-            entity0->getAudioSource()->play("thump.wav");
-            Inventory* opponentInv = (Inventory*)entity1->getComponent("inventory");
-            int ingredient = playerInventory->removeRandomPizzaIngredient(opponentInv->cheese, opponentInv->dough, opponentInv->sausage, opponentInv->tomato);
-            opponentInv->setIngredientFromId(ingredient);
-            //printf("%s collided with %s and transferred food id %d\n", entity0->name.c_str(), entity1->name.c_str(), ingredient);
-        }
+        entity0->checkPlayerCollision(entity1);
     }
     else if (entity1->name == "player1" || entity1->name == "player2" || entity1->name == "player3" || entity1->name == "player4")
     {
         Inventory* playerInventory = (Inventory*)entity1->getComponent("inventory");
-
         // FOOD 
-        if (entity0->name == "tomato" && playerInventory->tomato == 0)
-        {
-            playerInventory->tomato = 1;
-            entity1->getAudioSource()->play("pickup.wav");
-            //entity0->getModel()->visible = false;
-            entity0->removeFlag = true;
-        }
-        if (entity0->name == "cheese" && playerInventory->cheese == 0)
-        {
-            playerInventory->cheese = 1;
-            entity1->getAudioSource()->play("pickup.wav");
-            //entity0->getModel()->visible = false;
-            entity0->removeFlag = true;
-        }
-        if (entity0->name == "dough" && playerInventory->dough == 0)
-        {
-            playerInventory->dough = 1;
-            entity1->getAudioSource()->play("pickup.wav");
-            //entity0->getModel()->visible = false;
-            entity0->removeFlag = true;
-        }
-        if (entity0->name == "sausage" && playerInventory->sausage == 0)
-        {
-            playerInventory->sausage = 1;
-            entity1->getAudioSource()->play("pickup.wav");
-            //entity0->getModel()->visible = false;
-            entity0->removeFlag = true;
-        }
-        if (playerInventory->checkPizza())
-            entity1->getAudioSource()->play("ding.wav");
-
+        entity1->checkIngredientCollision(entity0);
         // OTHER PLAYERS
-        vector<string> otherPlayers = entity1->getOtherPlayers();
-        if (entity0->name == otherPlayers[0] || entity0->name == otherPlayers[1] || entity0->name == otherPlayers[2])
-        {
-            entity1->getAudioSource()->play("thump.wav");
-            Inventory* opponentInv = (Inventory*)entity0->getComponent("inventory");
-            int ingredient = playerInventory->removeRandomPizzaIngredient(opponentInv->cheese, opponentInv->dough, opponentInv->sausage, opponentInv->tomato);
-            opponentInv->setIngredientFromId(ingredient);
-            //printf("%s collided with %s and transferred food id %d\n", entity0->name.c_str(), entity1->name.c_str(), ingredient);
-        }
+        entity1->checkPlayerCollision(entity0);
+        
     }
 }
 
--- Bread/src/Scene/Entity.cpp
+++ Bread/src/Scene/Entity.cpp
@@ -1,4 +1,10 @@
+#include <string>
+#include <vector>
+
 #include "Entity.h"
+
+using namespace std;
+
 
 Entity::Entity(std::string name)
 {
@@ -70,4 +76,58 @@ std::vector<std::string> Entity::getOtherPlayers() {
 	}
 
 	return result;
+}
+
+void Entity::checkIngredientCollision(Entity* otherEntity) {
+	Inventory* playerInventory = (Inventory*)this->getComponent("inventory");
+
+	if (otherEntity->name == "tomato" && playerInventory->tomato == 0)
+	{
+		playerInventory->tomato = 1;
+		this->getAudioSource()->play("pickup.wav");
+		//entity0->getModel()->visible = false;
+		otherEntity->removeFlag = true;
+	}
+	if (otherEntity->name == "cheese" && playerInventory->cheese == 0)
+	{
+		playerInventory->cheese = 1;
+		this->getAudioSource()->play("pickup.wav");
+		//entity0->getModel()->visible = false;
+		otherEntity->removeFlag = true;
+	}
+	if (otherEntity->name == "dough" && playerInventory->dough == 0)
+	{
+		playerInventory->dough = 1;
+		this->getAudioSource()->play("pickup.wav");
+		//entity0->getModel()->visible = false;
+		otherEntity->removeFlag = true;
+	}
+	if (otherEntity->name == "sausage" && playerInventory->sausage == 0)
+	{
+		playerInventory->sausage = 1;
+		this->getAudioSource()->play("pickup.wav");
+		//entity0->getModel()->visible = false;
+		otherEntity->removeFlag = true;
+	}
+
+	// Check for completed recipes
+	if (playerInventory->checkPizza())
+		this->getAudioSource()->play("ding.wav");
+
+	return;
+}
+
+void Entity::checkPlayerCollision(Entity* otherEntity) {
+	Inventory* playerInventory = (Inventory*)this->getComponent("inventory");
+
+	vector<string> otherPlayers = this->getOtherPlayers();
+	if (otherEntity->name == otherPlayers[0] || otherEntity->name == otherPlayers[1] || otherEntity->name == otherPlayers[2])
+	{
+		this->getAudioSource()->play("thump.wav");
+		Inventory* opponentInv = (Inventory*)otherEntity->getComponent("inventory");
+		int ingredient = playerInventory->removeRandomPizzaIngredient(opponentInv->cheese, opponentInv->dough, opponentInv->sausage, opponentInv->tomato);
+		opponentInv->setIngredientFromId(ingredient);
+		//printf("%s collided with %s and transferred food id %d\n", entity0->name.c_str(), entity1->name.c_str(), ingredient);
+	}
+	return;
 }
