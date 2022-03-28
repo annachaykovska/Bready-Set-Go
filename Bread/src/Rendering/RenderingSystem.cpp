be0be4cede25f44115ce95c3be8f1f748438e6f6
ttdow <travis.t.dow@gmail.com>
2022-03-27 23:04:01 -0600

    Updated player models.

--- Bread/src/Rendering/RenderingSystem.cpp
+++ Bread/src/Rendering/RenderingSystem.cpp
@@ -210,7 +210,7 @@ skyboxShader("resources/shaders/skyboxVertex.txt", "resources/shaders/skyboxFrag
 	std::vector<std::string> faces;
 	faces.push_back("resources/textures/skybox/x.jpg");
 	faces.push_back("resources/textures/skybox/x.jpg");
-	faces.push_back("resources/textures/skybox/x.jpg");
+	faces.push_back("resources/textures/skybox/z.jpg");
 	faces.push_back("resources/textures/skybox/x.jpg");
 	faces.push_back("resources/textures/skybox/z.jpg");
 	faces.push_back("resources/textures/skybox/z.jpg");
@@ -235,8 +235,8 @@ skyboxShader("resources/shaders/skyboxVertex.txt", "resources/shaders/skyboxFrag
 	NavMesh navMeshPoints;
 	navMesh = navMeshPoints.getWireframe();
 
-	glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-	glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+	glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
 	glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
 	glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
 	glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_R, GL_CLAMP_TO_EDGE);
